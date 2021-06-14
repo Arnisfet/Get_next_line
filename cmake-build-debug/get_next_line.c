f7150ee415777f3eaedfcdc7e8fc68dfdd337b89
mrudge <Rohan9@mail.ru>
2021-06-14 21:17:11 +0300

    программа работает но есть утечки

--- cmake-build-debug/get_next_line.c
+++ cmake-build-debug/get_next_line.c
@@ -40,28 +40,31 @@ char *check_r(char *reminder, char **line)
 
 int get_next_line(int fd, char **line)
 {
-	char buf[10 + 1];
+	char buf[BUFFER_SIZE + 1];
 	int byte_was_read;
 	char *point;
 	static char *reminder;
-	char *mem_point;
 
+	if (fd < 0 || !line || BUFFER_SIZE < 1 || (read(fd, NULL, 0) < 0))
+		return (-1);
+	*line = "\0";
 	point = check_r(reminder, line);
 	byte_was_read = 1;
 	while (!point && byte_was_read)
 	{
-		byte_was_read  = read(fd, buf, 10);
+		byte_was_read  = read(fd, buf, BUFFER_SIZE);
 		buf[byte_was_read] = '\0';
-		if (point = ft_strchr(buf, '\n'))
+		point = ft_strchr(buf, '\n');
+		if (point)
 		{
 			*point = '\0';
 			reminder = ft_strdup(++point);
 		}
-		mem_point = *line;
 		*line = ft_strjoin(*line, buf);
-
+		if (*line)
+			free(*line);
 	}
-	if (byte_was_read || ft_strlen(reminder) || ft_strlen(line))
+	if (byte_was_read || ft_strlen(reminder) || ft_strlen(*line))
 		return (1);
 	return (0);
 }
--- cmake-build-debug/get_next_line.h
+++ cmake-build-debug/get_next_line.h
@@ -14,4 +14,5 @@ void	*ft_memcpy (void *dst, const void *src, size_t n);
 size_t ft_strlen(const char *dest);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strchr (const char *str, int ch);
+#define BUFFER_SIZE 42
 #endif //GET_NEXT_LINE_GET_NEXT_LINE_H
